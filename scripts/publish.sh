7e49d8af1e4d81a1b5b4660976445c438ad251fc
David Ng <david.ng.dev@gmail.com>
2020-08-23 08:26:26 +0800

    Revert "ci: remove hardcoded github email and name"

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,5 +2,7 @@
 
 set -ex
 
+git config user.email "github-action@github.com"
+git config user.name "GitHub Action"
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > ~/.npmrc
 HUSKY_SKIP_HOOKS=1 yarn lerna publish --yes
