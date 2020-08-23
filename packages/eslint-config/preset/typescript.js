f3e9a51b7ac95031a4fbe7db71430efc548f2e1e
David Ng <david.ng.dev@gmail.com>
2020-08-23 08:15:32 +0800

    feat(eslint-config): disable some typescript rules @typescript-eslint/camelcase @typescript-eslint/explicit-function-return-type @typescript-eslint/explicit-module-boundary-types @typescript-eslint/no-non-null-assertion @typescript-eslint/ban-ts-comment

--- packages/eslint-config/preset/typescript.js
+++ packages/eslint-config/preset/typescript.js
@@ -8,11 +8,32 @@ module.exports = {
       ],
       files: ['*.ts', '*.tsx'],
       parser: '@typescript-eslint/parser',
+      rules: {
+        // Disable this rule because when integrate third party the property name is out of control, E.X. it using snake_case
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+      },
     },
     {
       files: ['typings/**/*.d.ts'],
       rules: {
         '@typescript-eslint/no-unused-vars': 'off', // Not work for type declaration file
+      },
+    },
+    {
+      files: ['*.test.ts', '*.test.tsx'],
+      rules: {
+        '@typescript-eslint/ban-ts-comment': [
+          'error',
+          {
+            'ts-check': false,
+            'ts-expect-error': false,
+            'ts-ignore': true,
+            'ts-nocheck': true,
+          },
+        ],
       },
     },
   ],
