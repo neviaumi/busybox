0f6e832d9fda6f459c43b79b46c1c09dcd24de8a
David Ng <david.ng.dev@gmail.com>
2022-07-16 23:38:39 +0100

    feat(eslint-config): present of NestJS

---
+++ packages/eslint-config/preset/nest.js
@@ -0,0 +1,12 @@
+module.exports = {
+  overrides: [
+    {
+      files: ['*.ts'],
+      rules: {
+        'dot-notation': 'off',
+        'max-params': 'off',
+        'no-useless-constructor': 'off',
+      },
+    },
+  ],
+};
