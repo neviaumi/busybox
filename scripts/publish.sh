7fc03071748cd1790414f81f1e6e3bba20db7b6d
David Ng <david.ng.dev@gmail.com>
2022-09-16 23:24:43 +0100

    ISSUE-1162 use date version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,4 +8,14 @@ echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > ~/.npmrc
 npx lerna exec --stream -- "test ! -f  scripts/ci/pre-publish.sh || bash \
 scripts/ci/pre-publish.sh"
 
+if [-z "$PREFERRED_VERSION"]; then
+  VERSION=$PREFERRED_VERSION
+  if ["$GITHUB_REF_NAME" -eq "development"]; then
+    VERSION=$(date +'%Y.%-m.%-d-alpha.%-H%-M')
+  else
+    VERSION=$(date +'%Y.%-m.%-d')
+  fi
+fi
+HUSKY=0 npx lerna version --yes $VERSION
+
 HUSKY=0 npx lerna publish --yes from-git
