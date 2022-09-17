31232543f39d7a67c47ba950f486e5ea79843fd1
David Ng <david.ng.dev@gmail.com>
2022-09-17 01:15:15 +0100

    ISSUE-1162: avoid version have leading 0

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -13,7 +13,7 @@ else
   echo "Use date version"
   if [ "$GITHUB_REF_NAME" == "development" ]; then
     echo "Use date version on development"
-    VERSION=$(date +'%Y.%-m.%-d-alpha.%-H%-M')
+    VERSION=$(date +"%Y.%-m.%-d-alpha.$(($(date +"%-H") + 1))%-M'")
   else
     echo "Use date version on master"
     VERSION=$(date +'%Y.%-m.%-d')
