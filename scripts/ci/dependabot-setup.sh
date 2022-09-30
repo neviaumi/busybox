7dd8b660830bf652448287ea30408f40783b1e1a
David Ng <david.ng.dev@gmail.com>
2022-09-30 18:18:06 +0100

    fix: missing git user email and name

--- scripts/ci/dependabot-setup.sh
+++ scripts/ci/dependabot-setup.sh
@@ -7,6 +7,8 @@ git diff --exit-code
 GIT_DIFF_EXIST_CODE=$?
 set -e
 if [ $GIT_DIFF_EXIST_CODE -eq 1 ]; then
+  git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+  git config user.name "github-actions[bot]"
   git add .
   git commit --fixup HEAD
   git push
