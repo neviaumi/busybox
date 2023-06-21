29505283c135cd93ec4cc07b9254602387d83a21
davidnhk <david.ng.dev@gmail.com>
2023-06-21 17:22:19 +0100

    ISSUE-1472: udpate lerna 7.0

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -10,7 +10,6 @@ echo "Current branch is $CURRENT_BRANCH"
 CURRENT_WORKING_DIRECTORY=$(pwd)
 
 npm ci
-npx lerna bootstrap
 npx lerna exec --concurrency 1 --stream -- "test ! -f  scripts/ci/setup.sh || bash \
 scripts/ci/setup.sh"
 npm install --no-save --no-package-lock "@busybox/commitlint-config@./packages/commitlint-config" "@busybox/eslint-config@./packages/eslint-config" "@busybox/prettier-config@./packages/prettier-config"
