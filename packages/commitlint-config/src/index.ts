c9f164716cfdf48445a0bf018c662c9645a2fe87
davidnhk <david.ng.dev@gmail.com>
2023-06-22 01:48:13 +0100

    ISSUE-1479: commitlint request body
    this close #1479

--- packages/commitlint-config/src/index.ts
+++ packages/commitlint-config/src/index.ts
@@ -1,9 +1,12 @@
 import type { UserConfig } from '@commitlint/types';
 
+import regexMatch from './plugins/regex-match/index.js';
+
 const Configuration: UserConfig = {
   extends: ['jira'],
-  plugins: ['commitlint-plugin-jira-rules'],
+  plugins: ['commitlint-plugin-jira-rules', regexMatch],
   rules: {
+    'body-match': [2, 'always', /^this close #(?<issueId>\d+)$/],
     'jira-task-id-max-length': [2, 'always', 5 + 1 + 6],
     'jira-task-id-project-key': [2, 'always', 'ISSUE'],
   },
