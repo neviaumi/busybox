b5ef60389cd687ce3d8b87ee2003287c5ee56504
davidnhk <david.ng.dev@gmail.com>
2023-07-13 02:11:47 +0100

    ISSUE-1535: check body and header issue id is match
    this fix #1535

--- packages/commitlint-config/src/index.ts
+++ packages/commitlint-config/src/index.ts
@@ -6,9 +6,9 @@ const Configuration: UserConfig = {
   extends: ['jira'],
   plugins: ['commitlint-plugin-jira-rules', regexMatch],
   rules: {
-    'body-match': [2, 'always', /^this close #(?<issueId>\d+)$/],
     'jira-task-id-max-length': [2, 'always', 5 + 1 + 6],
     'jira-task-id-project-key': [2, 'always', 'ISSUE'],
+    'linking-to-github-issue': [2, 'always'],
   },
 };
 
