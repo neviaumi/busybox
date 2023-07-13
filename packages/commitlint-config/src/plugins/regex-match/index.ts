7e34a04cffb8d1ebb952ba352548a0635a3fc65d
davidnhk <david.ng.dev@gmail.com>
2023-07-13 02:11:47 +0100

    ISSUE-1535: check body and header issue id is match
    this fix #1535

--- packages/commitlint-config/src/plugins/regex-match/index.ts
+++ packages/commitlint-config/src/plugins/regex-match/index.ts
@@ -2,18 +2,26 @@ import type { Plugin } from '@commitlint/types';
 
 const commitLintPlugin: Plugin = {
   rules: {
-    'body-match': (parsed, _, regex) => {
-      const { body, footer } = parsed;
+    'linking-to-github-issue': parsed => {
+      // https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword
+      const bodyRegex =
+        /^this (close|closes|closed|fix|fixes|fixed|resolve|resolves|resolved) #(?<issueId>\d+)$/i;
+      const headerRegex = /^ISSUE-(?<issueId>\d+): .*$/;
+      const { body, footer, header } = parsed;
       const messageBody = body || footer;
-      if (!regex) {
-        return [true];
+      const isValid = messageBody !== null && bodyRegex.test(messageBody);
+      if (!isValid) {
+        return [
+          isValid,
+          `commit message body (${messageBody}) doest not match with pattern : ${bodyRegex}`,
+        ];
       }
-      const isValid =
-        messageBody !== null && new RegExp(regex, 'ig').test(messageBody);
+      const bodyIssueId = messageBody.match(bodyRegex)?.groups?.['issueId'];
+      const headerIssueId = header.match(headerRegex)?.groups?.['issueId'];
 
       return [
-        isValid,
-        `commit message body (${messageBody}) doest not match with pattern : ${regex}`,
+        bodyIssueId === headerIssueId,
+        `issue id ${bodyIssueId} specific in body not equal to ${headerIssueId} in header`,
       ];
     },
   },
