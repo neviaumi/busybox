8cc098862b4a1d5581e63e8b42e0abaf97699bd8
davidnhk <david.ng.dev@gmail.com>
2023-11-11 00:41:05 +0000

    ISSUE-1748: adjust color schema and seperate border color config
    this close #1748

--- packages/tailwindcss-config/tailwind.config.ts
+++ packages/tailwindcss-config/tailwind.config.ts
@@ -25,27 +25,37 @@ export const withColors = pipe(
     pipe(
       extractColor(['current']),
       extractColor(['transparent']),
+      extractColor(['rose', '200']),
+      extractColor(['rose', '300']),
       extractColor(['rose', '500']),
       extractColor(['rose', '600']),
+      extractColor(['rose', '800']),
+      extractColor(['rose', '900']),
       extractColor(['gray']),
       extractColor(['white']),
       extractColor(['black']),
+      extractColor(['amber', '200']),
+      extractColor(['amber', '300']),
       extractColor(['amber', '500']),
       extractColor(['amber', '600']),
+      extractColor(['amber', '800']),
+      extractColor(['amber', '900']),
       extractColor(['emerald', '50']),
       extractColor(['emerald', '100']),
-      extractColor(['sky', '500']),
-      extractColor(['sky', '600']),
+      extractColor(['emerald', '200']),
+      extractColor(['emerald', '300']),
+      extractColor(['sky', '50']),
+      extractColor(['sky', '100']),
+      extractColor(['sky', '200']),
+      extractColor(['sky', '300']),
+      extractColor(['sky', '800']),
+      extractColor(['sky', '900']),
     )({}),
   ),
   assocPath(['theme', 'borderColor'], ({ theme }: PluginUtils) => {
-    return theme('textColor');
-  }),
-  assocPath(['theme', 'backgroundColor'], ({ theme }: PluginUtils) => {
     return pipe(
       assocPath([Variant.DISABLED], {
         DEFAULT: theme('colors.gray.200'),
-        hover: theme('colors.gray.50'),
       }),
       assocPath([Variant.ERROR], {
         DEFAULT: theme('colors.rose.500'),
@@ -56,36 +66,58 @@ export const withColors = pipe(
         hover: theme('colors.amber.600'),
       }),
       assocPath([Variant.PRIMARY], {
+        DEFAULT: theme('colors.emerald.200'),
+        hover: theme('colors.emerald.300'),
+      }),
+      assocPath([Variant.SECONDARY], {
+        DEFAULT: theme('colors.sky.200'),
+        hover: theme('colors.sky.300'),
+      }),
+    )(theme('colors'));
+  }),
+  assocPath(['theme', 'backgroundColor'], ({ theme }: PluginUtils) => {
+    return pipe(
+      assocPath([Variant.DISABLED], {
+        DEFAULT: theme('colors.gray.100'),
+      }),
+      assocPath([Variant.ERROR], {
+        DEFAULT: theme('colors.rose.200'),
+        hover: theme('colors.rose.300'),
+      }),
+      assocPath([Variant.WARNING], {
+        DEFAULT: theme('colors.amber.200'),
+        hover: theme('colors.amber.300'),
+      }),
+      assocPath([Variant.PRIMARY], {
         DEFAULT: theme('colors.emerald.50'),
         hover: theme('colors.emerald.100'),
       }),
       assocPath([Variant.SECONDARY], {
-        DEFAULT: theme('colors.sky.500'),
-        hover: theme('colors.sky.600'),
+        DEFAULT: theme('colors.sky.50'),
+        hover: theme('colors.sky.100'),
       }),
     )(theme('colors'));
   }),
   assocPath(['theme', 'textColor'], ({ theme }: PluginUtils) => {
     return pipe(
       assocPath([Variant.DISABLED], {
-        DEFAULT: theme('colors.gray.800'),
-        hover: theme('colors.gray.600'),
+        DEFAULT: theme('colors.gray.400'),
       }),
       assocPath([Variant.ERROR], {
-        DEFAULT: theme('colors.gray.50'),
-        hover: theme('colors.white'),
+        DEFAULT: theme('colors.rose.800'),
+        hover: theme('colors.rose.900'),
       }),
       assocPath([Variant.WARNING], {
-        DEFAULT: theme('colors.gray.50'),
-        hover: theme('colors.white'),
+        DEFAULT: theme('colors.amber.800'),
+        hover: theme('colors.amber.900'),
       }),
       assocPath([Variant.PRIMARY], {
-        DEFAULT: theme('colors.gray.700'),
-        hover: theme('colors.black'),
+        DEFAULT: theme('colors.gray.800'),
+        hover: theme('colors.gray.900'),
       }),
       assocPath([Variant.SECONDARY], {
-        DEFAULT: theme('colors.gray.50'),
-        hover: theme('colors.white'),
+        DEFAULT: theme('colors.sky.800'),
+        hover: theme('colors.sky.900'),
       }),
     )(theme('colors'));
   }),
