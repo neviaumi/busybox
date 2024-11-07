5954bf402584e53dae3b8049f5e43aa7d492275d
David Ng <david.ng.dev@gmail.com>
2024-11-07 16:07:49 +0000

    GH-2311: Remove Cypress and Storybook ESLint configs
    this part of #2311
    Removed ESLint configurations related to Cypress and Storybook to streamline the setup. Updated `package.json` and other related files to reflect these changes. Adjusted the dependencies in `package-lock.json` accordingly to remove unused packages.

--- packages/eslint-config/index.mjs
+++ packages/eslint-config/index.mjs
@@ -1,66 +1,42 @@
 import eslintGlobals from 'globals';
 
-import cypressConfig from './preset/cypress.mjs';
 import jsCommonConfig from './preset/javascript.mjs';
 import jestConfig from './preset/jest.mjs';
 import jsonConfig from './preset/json.mjs';
 import markdownConfig from './preset/markdown.mjs';
-import nestConfig from './preset/nest.mjs';
 import ymlConfig from './preset/yml.mjs';
 import { hasConfig } from './utils/has-config.mjs';
 import { isDefaultEsm } from './utils/is-default-esm.mjs';
-
-const hasNest = await hasConfig([
-  { dependency: '@nestjs/core', type: 'dependency' },
-  { dependency: '@nestjs/core', dependencyType: 'peer', type: 'dependency' },
-]);
 
 const hasJest = await hasConfig([
   { dependency: 'jest', dependencyType: 'dev', type: 'dependency' },
 ]);
 
-const hasStorybook = await hasConfig([
-  { dependency: 'storybook', dependencyType: 'dev', type: 'dependency' },
-]);
-
-const hasCypress = await hasConfig([
-  { dependency: 'cypress', dependencyType: 'dev', type: 'dependency' },
-]);
-
 const isDefaultESModule = await isDefaultEsm();
 
-function createEsLintConfig({ hasCypress, hasJest, hasNest }) {
+function createEsLintConfig({ hasJest }) {
   const eslintConfig = [
     jsCommonConfig,
     jsonConfig,
     markdownConfig,
     ymlConfig,
     hasJest ? jestConfig : [],
-    hasCypress ? cypressConfig : [],
-    hasNest ? nestConfig : [],
   ].flat();
   return eslintConfig;
 }
 
 export default createEsLintConfig({
-  hasCypress,
   hasJest,
-  hasNest,
-  hasStorybook,
 });
 
 export function withOverridePackageAutoDetect(overrides = {}) {
   const {
-    hasCypress: hasCypressOverride = hasCypress,
     hasJest: hasJestOverride = hasJest,
-    hasNest: hasNestOverride = hasNest,
     isDefaultESModule: isDefaultESModuleOverride = isDefaultESModule,
   } = overrides;
   return () => {
     return createEsLintConfig({
-      hasCypress: hasCypressOverride,
       hasJest: hasJestOverride,
-      hasNest: hasNestOverride,
       isDefaultESModule: isDefaultESModuleOverride,
     });
   };
