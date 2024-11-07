222e75250cf8d48620c20da0e041a45ed0bcd39a
David Ng <david.ng.dev@gmail.com>
2024-11-07 17:49:17 +0000

    GH-2311: Modularize Jest ESLint config
    this part of #2311
    
    Moved Jest ESLint configurations to a new package. Introduced @busybox/eslint-config-jest and refactored related imports and dependencies accordingly.

--- packages/eslint-config/index.mjs
+++ packages/eslint-config/index.mjs
@@ -1,36 +1,12 @@
 import eslintGlobals from 'globals';
 
 import jsCommonConfig from './preset/javascript.mjs';
-import jestConfig from './preset/jest.mjs';
-import { hasConfig } from './utils/has-config.mjs';
-import { isDefaultEsm } from './utils/is-default-esm.mjs';
 
-const hasJest = await hasConfig([
-  { dependency: 'jest', dependencyType: 'dev', type: 'dependency' },
-]);
-
-const isDefaultESModule = await isDefaultEsm();
-
-function createEsLintConfig({ hasJest }) {
-  const eslintConfig = [jsCommonConfig, hasJest ? jestConfig : []].flat();
+function createEsLintConfig() {
+  const eslintConfig = [jsCommonConfig].flat();
   return eslintConfig;
 }
 
-export default createEsLintConfig({
-  hasJest,
-});
-
-export function withOverridePackageAutoDetect(overrides = {}) {
-  const {
-    hasJest: hasJestOverride = hasJest,
-    isDefaultESModule: isDefaultESModuleOverride = isDefaultESModule,
-  } = overrides;
-  return () => {
-    return createEsLintConfig({
-      hasJest: hasJestOverride,
-      isDefaultESModule: isDefaultESModuleOverride,
-    });
-  };
-}
+export default createEsLintConfig({});
 
 export const globals = eslintGlobals;
